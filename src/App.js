4ddb8470a13b3984cd0978a06822b3edae184156
Yufie23 <gonzalo.ain.ortiz@gmail.com>
2022-01-19 20:47:17 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './fondo.jpeg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primera pagina con React.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/GonzaOrtiz96?tab=repositories"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mis trabajos finales de otros cursos
         </a>
       </header>
     </div>
