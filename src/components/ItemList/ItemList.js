c6efa3431982090989f45e5da6a039ac859e40a1
Yufie23 <gonzalo.ain.ortiz@gmail.com>
2022-02-07 17:50:03 -0300

    Re-entrega de MAPS y Promises

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,22 +2,16 @@ import React, {useState, useEffect} from 'react';
 import Item from '../Item/item';
 import '../bootstrap.min.css';
 
-const ItemList = () =>{
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetch('https://api.github.com/users')
-        .then((response) => response.json())
-        .then((json) => setUsers(json));
-    }, []);
+const ItemList = ({datos}) =>{
+    const [guardado] = useState({datos});
     return(
         <div className='card col'>
-            {users.map((info) => {
+            {datos.map((info) =>{
                 return (
                     <div key={info.id}>
                         <Item data={info}/>
                     </div>
-                );
+                )
             })}
         </div>
     );
--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,11 +1,19 @@
+import React, {useState, useEffect} from 'react';
 import './bootstrap.min.css';
 import ItemList from './ItemList/ItemList';
 
-function ListContainer(props) {
+const ListContainer = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users')
+        .then((response) => response.json())
+        .then((json) => setUsers(json));
+    }, []);
     return(
         <div className='container'>
             <div className='row'>
-                <ItemList />
+                <ItemList datos={users}/>
             </div>            
         </div>    
     );
